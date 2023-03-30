6c745bb84b68628b3a11774edf58dce4ea035f71
nmhieu0504 <84891428+nmhieu0504@users.noreply.github.com>
2023-03-30 20:21:54 +0700

    update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_gpt/widgets/chat_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true
-      ),
-      home: const ChatPage()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true),
+        home: const ChatPage());
   }
 }
--- lib/widgets/chat_page.dart
+++ lib/widgets/chat_page.dart
@@ -55,164 +55,197 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Chat GPT"),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              const DrawerHeader(
+        title: const Text("Chat GPT"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const SizedBox(
+              height: 100,
+              child: DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  // color: Colors.blue,
                 ),
                 child: Text(
-                  'Drawer Header',
+                  'Settings',
                   style: TextStyle(
-                    color: Colors.white,
+                    // color: Colors.white,
                     fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              ListTile(
-                leading: const Icon(Icons.play_circle_outline),
-                title: const Text('Auto TTS reply'),
-                trailing: Switch(
-                  value: true,
-                  onChanged: (value) {},
-                ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.play_circle_outline),
+              title: const Text('Auto TTS reply'),
+              trailing: Switch(
+                value: true,
+                onChanged: (value) {},
               ),
-              ListTile(
-                leading: const Icon(Icons.language_outlined),
-                title: const Text('Speech Language'),
-                onTap: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => Dialog(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Card(
-                            clipBehavior: Clip.hardEdge,
-                            child: InkWell(
-                              splashColor: Colors.blue.withAlpha(30),
-                              onTap: () {
-                                debugPrint('Card tapped.');
-                              },
-                              child: Card(
-                                child: ListTile(
-                                  leading: SizedBox(
-                                      width: 50,
-                                      height: 50,
-                                      child: Image.asset(
-                                          'assets/images/us_flag.png')),
-                                  title: const Text('English (United States)'),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Card(
-                            clipBehavior: Clip.hardEdge,
-                            child: InkWell(
-                              splashColor: Colors.blue.withAlpha(30),
-                              onTap: () {
-                                debugPrint('Card tapped.');
-                              },
-                              child: Card(
-                                child: ListTile(
-                                  leading: SizedBox(
-                                      width: 50,
-                                      height: 50,
-                                      child: Image.asset(
-                                          'assets/images/vn_flag.png')),
-                                  title: const Text('Vietnamese'),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 15),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
+            ),
+            ListTile(
+              leading: const Icon(Icons.language_outlined),
+              title: const Text('Speech Language'),
+              onTap: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => Dialog(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Card(
+                          clipBehavior: Clip.hardEdge,
+                          child: InkWell(
+                            splashColor: Colors.blue.withAlpha(30),
+                            onTap: () {
+                              debugPrint('Card tapped.');
                             },
-                            child: const Text('Close'),
+                            child: ListTile(
+                              leading: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child:
+                                      Image.asset('assets/images/us_flag.png')),
+                              title: const Text('English (United States)'),
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.hardEdge,
+                          child: InkWell(
+                            splashColor: Colors.blue.withAlpha(30),
+                            onTap: () {
+                              debugPrint('Card tapped.');
+                            },
+                            child: ListTile(
+                              leading: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child:
+                                      Image.asset('assets/images/vn_flag.png')),
+                              title: const Text('Vietnamese'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Close'),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: FilledButton(
-                    onPressed: () {}, child: const Text('Delete all messages')),
-              ),
-            ],
-          ),
-        ),
-        body: SafeArea(
-          child: Column(children: [
-            Expanded(
-              child: GroupedListView<Message, DateTime>(
-                order: GroupedListOrder.DESC,
-                reverse: true,
-                floatingHeader: true,
-                padding: const EdgeInsets.all(10),
-                elements: messageList,
-                groupBy: (message) => DateTime(
-                  message.date.year,
-                  message.date.month,
-                  message.date.day,
-                ),
-                groupHeaderBuilder: (Message message) => Container(
-                  margin: const EdgeInsets.only(bottom: 10),
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Text(DateFormat.yMMMMEEEEd().format(message.date),
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 11.0)),
-                  ),
-                ),
-                itemBuilder: (context, message) => Bubble(
-                    margin: const BubbleEdges.only(top: 5, bottom: 5),
-                    alignment: message.isUser
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    nip:
-                        message.isUser ? BubbleNip.rightTop : BubbleNip.leftTop,
-                    color: message.isUser ? Colors.blue : Colors.grey,
-                    child: Text(
-                      message.message,
-                      style: const TextStyle(color: Colors.white),
-                    )),
-              ),
             ),
             Container(
-              margin: const EdgeInsets.all(30),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                  hintText: "Typing something ...",
-                  contentPadding:
-                      EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                ),
-                controller: textFieldController,
-                onSubmitted: (value) {
-                  final message = Message(
-                      message: value, date: DateTime.now(), isUser: true);
-                  textFieldController.clear();
-                  setState(() => messageList.add(message));
-                },
-              ),
-            )
-          ]),
+              margin: const EdgeInsets.all(20),
+              child: FilledButton(
+                  onPressed: () {}, child: const Text('Delete all messages')),
+            ),
+          ],
         ),
-        bottomNavigationBar: const BottomAppBar(
-          shape: CircularNotchedRectangle(),
-        ));
+      ),
+      body: SafeArea(
+        child: Column(children: [
+          Expanded(
+            child: GroupedListView<Message, DateTime>(
+              order: GroupedListOrder.DESC,
+              reverse: true,
+              // floatingHeader: true,
+              padding: const EdgeInsets.all(10),
+              elements: messageList,
+              groupBy: (message) => DateTime(
+                message.date.year,
+                message.date.month,
+                message.date.day,
+              ),
+              groupHeaderBuilder: (Message message) => Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(DateFormat.yMMMMEEEEd().format(message.date),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 11.0)),
+                ),
+              ),
+              itemBuilder: (context, message) => Bubble(
+                  margin: const BubbleEdges.only(top: 5, bottom: 5),
+                  alignment: message.isUser
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  nip: message.isUser ? BubbleNip.rightTop : BubbleNip.leftTop,
+                  color: message.isUser ? Colors.blue : Colors.grey[200],
+                  child: Text(
+                    message.message,
+                    style: TextStyle(
+                        color: message.isUser ? Colors.white : Colors.black),
+                  )),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40.0))),
+                hintText: "Typing something ...",
+                contentPadding:
+                    EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+              ),
+              controller: textFieldController,
+              onSubmitted: (value) {
+                final message =
+                    Message(message: value, date: DateTime.now(), isUser: true);
+                textFieldController.clear();
+                setState(() => messageList.add(message));
+              },
+            ),
+          )
+        ]),
+      ),
+      bottomNavigationBar: Container(
+        width: 90,
+        height: 90,
+        margin: const EdgeInsets.all(10),
+        child: Column(children: [
+          IconButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(const CircleBorder()),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.blue), // <-- Button color
+              ),
+              icon: const Icon(
+                Icons.mic_outlined,
+                color: Colors.white,
+              )),
+          const Text(
+            "Tap to Talk",
+            style: TextStyle(
+                fontStyle: FontStyle.italic, fontSize: 18, color: Colors.black),
+          )
+        ]),
+      ),
+    );
   }
 }
